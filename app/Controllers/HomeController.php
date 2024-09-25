811e865cb6307ecf24ad69a2b24bfbe234ef907b
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-25 12:09:37 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -556,7 +556,7 @@ class HomeController
 		$html_template = '
 		<div class="card">
 		<div class="card-body bg-dark">
-			<h5 class="card-title">Generador de Módulos</h5>
+			<h5 class="card-title mb-0">Generador de Módulos</h5>
 		</div>
 		<div class="card-body bg-light">
 
