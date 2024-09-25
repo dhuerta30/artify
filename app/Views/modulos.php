dddb85e3041b44f363d103be3e25aa8d2fca633d
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-25 12:02:33 -0300

    up

--- app/Views/modulos.php
+++ app/Views/modulos.php
@@ -121,7 +121,7 @@ $(document).on("pdocrud_after_ajax_action",function(event, obj, data){
             dataType: "json",
             success: function(data){
                 console.log(data);
-                $.each(data["columnas_tabla"], function(index, obj) {  
+                $.each(data["columnas_tabla"], function(index, obj) {
                     // Acceder al nombre del campo y su tipo
                     var campo = obj.Field;
                     var tipo = obj.Type;
