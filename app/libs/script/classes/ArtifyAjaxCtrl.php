d9994709ce10a1fd3b2eb8510186d923d2f3f097
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-26 16:07:46 -0300

    up

--- app/libs/script/classes/ArtifyAjaxCtrl.php
+++ app/libs/script/classes/ArtifyAjaxCtrl.php
@@ -9,8 +9,8 @@ Class ArtifyAjaxCtrl {
             die("La sesión ha caducado. Actualice su página para continuar.");
         }
 
-        $pdocrud = @unserialize($_SESSION["pdocrud_sess"][$instanceKey]);
-        if ($pdocrud === false) {
+        $artify = @unserialize($_SESSION["pdocrud_sess"][$instanceKey]);
+        if ($artify === false) {
             die("Ocurrió un error. Por favor, inténtelo de nuevo más tarde.");
         }
 
@@ -23,200 +23,200 @@ Class ArtifyAjaxCtrl {
         $data["post"] = $post;
         switch (strtoupper($action)) {
             case "VIEW":
-                echo $pdocrud->render("VIEWFORM", $data);
+                echo $artify->render("VIEWFORM", $data);
                 break;
             case "SORT":
-                $pdocrud->setBackOperation();
+                $artify->setBackOperation();
                 $data["action"] = "asc";
-                echo $pdocrud->render("CRUD", $data);
+                echo $artify->render("CRUD", $data);
                 break;
             case "ASC":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "DESC":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "ADD":
-                echo $pdocrud->render("INSERTFORM", $data);
+                echo $artify->render("INSERTFORM", $data);
                 break;
             case "INSERT":
-                $pdocrud->render("INSERT", $post);
+                $artify->render("INSERT", $post);
                 break;
             case "INSERT_CLOSE":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("INSERT", $post);
+                $artify->setBackOperation();
+                $artify->render("INSERT", $post);
                 break;
             case "INSERT_BACK":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("INSERT", $post);
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->render("INSERT", $post);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "BACK":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "EDIT":
-                $pdocrud->setInlineEdit(false);
-                echo $pdocrud->render("EDITFORM", $data);
+                $artify->setInlineEdit(false);
+                echo $artify->render("EDITFORM", $data);
                 break;
             case "INLINE_EDIT":
-                $pdocrud->setBackOperation();
-                $pdocrud->setInlineEdit(true);
-                echo $pdocrud->render("EDITFORM", $data);
+                $artify->setBackOperation();
+                $artify->setInlineEdit(true);
+                echo $artify->render("EDITFORM", $data);
                 break;
             case "ONEPAGEEDIT":
-                $pdocrud->setBackOperation();
-                $pdocrud->setInlineEdit(false);
-                echo $pdocrud->render("ONEPAGE", $data);
+                $artify->setBackOperation();
+                $artify->setInlineEdit(false);
+                echo $artify->render("ONEPAGE", $data);
                 break;
             case "ONEPAGEVIEW":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("ONEPAGE", $data);
+                $artify->setBackOperation();
+                echo $artify->render("ONEPAGE", $data);
                 break;
             case "QUICK_VIEW":
-                echo $pdocrud->render("QUICKVIEW", $data);
+                echo $artify->render("QUICKVIEW", $data);
                 break;
             case "RELATED_TABLE":
-                echo $pdocrud->render("RELATED_TABLE", $data);
+                echo $artify->render("RELATED_TABLE", $data);
                 break;
             case "INLINE_BACK":
-                $pdocrud->render("UPDATE", $post);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->render("UPDATE", $post);
+                echo $artify->render("CRUD", $data);
                 break;
             case "UPDATE":
-                $pdocrud->render("UPDATE", $post);
+                $artify->render("UPDATE", $post);
                 break;
             case "UPDATE_BACK":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("UPDATE", $post);
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->render("UPDATE", $post);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "UPDATE_CLOSE":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("UPDATE", $post);
+                $artify->setBackOperation();
+                $artify->render("UPDATE", $post);
                 break;
             case "DELETE":
-                $pdocrud->render("DELETE", $data);
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->render("DELETE", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "DELETE_SELECTED":
-                $pdocrud->render("DELETE_SELECTED", $data);
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->render("DELETE_SELECTED", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "PAGINATION":
                 if($data["rendertype"] == "CRUD"){
-                    $pdocrud->setBackOperation();
-                    $pdocrud->currentPage($data["page"]);
-                    echo $pdocrud->render("CRUD", $data);
+                    $artify->setBackOperation();
+                    $artify->currentPage($data["page"]);
+                    echo $artify->render("CRUD", $data);
                 } else {
-                    $pdocrud->currentPage($data["page"]);
-                    echo $pdocrud->render("SQL", $data);
+                    $artify->currentPage($data["page"]);
+                    echo $artify->render("SQL", $data);
                 }
                 break;
             case "RECORDS_PER_PAGE":
                 if($data["rendertype"] == "CRUD"){
-                    $pdocrud->currentPage(1);
-                    $pdocrud->recordsPerPage($data["records"]);
-                    echo $pdocrud->render("CRUD", $data);
+                    $artify->currentPage(1);
+                    $artify->recordsPerPage($data["records"]);
+                    echo $artify->render("CRUD", $data);
                 } else {
-                    $pdocrud->currentPage(1);
-                    $pdocrud->recordsPerPage($data["records"]);
-                    echo $pdocrud->render("SQL", $data);
+                    $artify->currentPage(1);
+                    $artify->recordsPerPage($data["records"]);
+                    echo $artify->render("SQL", $data);
                 }
                 break;
             case "SEARCH":
                 if($data["rendertype"] == "CRUD"){
-                    $pdocrud->currentPage(1);
-                    echo $pdocrud->render("CRUD", $data);
+                    $artify->currentPage(1);
+                    echo $artify->render("CRUD", $data);
                 } else {
-                    $pdocrud->currentPage(1);
-                    echo $pdocrud->render("SQL", $data);
+                    $artify->currentPage(1);
+                    echo $artify->render("SQL", $data);
                 }
                 break;
             case "AUTOSUGGEST":
                 if (isset($_GET["callback"])) {
                     $data["callback"] = filter_var($_GET["callback"], FILTER_SANITIZE_STRING);
                 }
-                echo $pdocrud->render("AUTOSUGGEST", $data);
+                echo $artify->render("AUTOSUGGEST", $data);
                 break;
             case "EXPORTTABLE":
-                echo $pdocrud->render("EXPORTTABLE", $data);
+                echo $artify->render("EXPORTTABLE", $data);
                 break;
             case "EXPORTFORM":
-                $pdocrud->render("EXPORTFORM", $data);
+                $artify->render("EXPORTFORM", $data);
                 break;
             case "SWITCH":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("SWITCH", $data);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->render("SWITCH", $data);
+                echo $artify->render("CRUD", $data);
                 break;
             case "BTNSWITCH":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("BTNSWITCH", $data);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->render("BTNSWITCH", $data);
+                echo $artify->render("CRUD", $data);
                 break;
             case "LOADDEPENDENT":
-                echo $pdocrud->render("LOADDEPENDENT", $data);
+                echo $artify->render("LOADDEPENDENT", $data);
                 break;
-            case "EMAIL" : echo $pdocrud->render("EMAIL", $post);
+            case "EMAIL" : echo $artify->render("EMAIL", $post);
                 break;
             case "SELECT":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "SELECTFORM":
-                echo $pdocrud->render("SELECT", $post);
+                echo $artify->render("SELECT", $post);
                 break;
             case "FILTER":
-                $pdocrud->setBackOperation();
-                $pdocrud->currentPage(1);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->currentPage(1);
+                echo $artify->render("CRUD", $data);
                 break;
             case "REFRESH":
                 if($data["rendertype"] == "CRUD"){
-                    echo $pdocrud->render("CRUD", $data);
+                    echo $artify->render("CRUD", $data);
                 } else {
-                    echo $pdocrud->render("SQL", $data);
+                    echo $artify->render("SQL", $data);
                 }
                 break;
             case "RELOAD":
-                $pdocrud->setBackOperation();
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                echo $artify->render("CRUD", $data);
                 break;
             case "SAVE_CRUD_TABLE_DATA":
-                $pdocrud->setBackOperation();
-                $pdocrud->render("SAVE_CRUD_DATA", $data);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->render("SAVE_CRUD_DATA", $data);
+                echo $artify->render("CRUD", $data);
                 break;
             case "RENDER_ADV_SEARCH":
-                $pdocrud->currentPage(1);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->currentPage(1);
+                echo $artify->render("CRUD", $data);
                 break;
             case "DATE_RANGE_REPORT":
-                $pdocrud->setBackOperation();
-                $pdocrud->currentPage(1);
-                echo $pdocrud->render("CRUD", $data);
+                $artify->setBackOperation();
+                $artify->currentPage(1);
+                echo $artify->render("CRUD", $data);
                 break;
             case "CLONE":
-                echo $pdocrud->render("CLONEFORM", $data);
+                echo $artify->render("CLONEFORM", $data);
                 break;
             case "CELL_UPDATE":
                 $updateData[$data["column"]] = $data["content"];
                  if (isset($data["id"]))
-                    $pdocrud->setPrimarykeyValue($data["id"]);
-                $pdocrud->render("UPDATE", $updateData);
+                    $artify->setPrimarykeyValue($data["id"]);
+                $artify->render("UPDATE", $updateData);
                 break;
             case "AJAX_ACTION":
-                echo $pdocrud->render("AJAX_ACTION", $data);
+                echo $artify->render("AJAX_ACTION", $data);
                 break;   
             case "PRINTPDF":
-                echo $pdocrud->render("PRINT_PDF", $data);
+                echo $artify->render("PRINT_PDF", $data);
                 break;   
             default:
                 break;
