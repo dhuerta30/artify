d12c6c92146ea698b12a9c6ce26650ae09708f8d
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-26 16:40:43 -0300

    up

--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -82,7 +82,7 @@
                     <?php endforeach; ?>
 
                     <li class="nav-item">
-                        <a href="javascript:;" class="nav-link"><i class="fa fa-book"></i> Documentación</a>
+                        <a href="javascript:;" class="nav-link" data-toggle="modal" data-target="#Documentation"><i class="fa fa-book"></i> Documentación</a>
                     </li>
 
                     </ul>
@@ -93,3 +93,26 @@
      </div>
      <!-- /.sidebar -->
  </aside>
+
+
+
+ <div class="modal fade" id="Documentation" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-book"></i> Documentación</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       
+    
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
