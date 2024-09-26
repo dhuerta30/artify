ae3b92b3fbdab6b15b636f0bc18ebb1ed17eef70
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-09-26 11:36:37 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -898,6 +898,7 @@ class HomeController
 
 							<div class="row mt-4">
 								<div class="col-md-12 text-center">
+									<button type="button" class="btn btn-danger pdocrud-form-control pdocrud-button pdocrud-back" name="pdocrud_submit_19MsSO3Edq_back" data-action="back">Regresar</button> 
 									<a href="javascript:;" class="btn btn-primary siguiente_1">Siguiente <i class="fa fa-arrow-right"></i></a>
 								</div>
 							</div>
@@ -935,6 +936,7 @@ class HomeController
 
 				<div class="row mt-4">
 					<div class="col-md-12 text-center">
+						<button type="button" class="btn btn-danger pdocrud-form-control pdocrud-button pdocrud-back" name="pdocrud_submit_19MsSO3Edq_back" data-action="back">Regresar</button> 
 						<a href="javascript:;" class="btn btn-primary anterior"><i class="fa fa-arrow-left"></i> Anterior</a>
 						<a href="javascript:;" class="btn btn-primary siguiente_2">Siguiente <i class="fa fa-arrow-right"></i></a>
 					</div>
