ff7f7a60651ef7c5ac45aaf3c2082285cdd6dcc9
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-02 09:08:36 -0300

    up

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
+//ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
+//error_reporting(-1);
 date_default_timezone_set(@date_default_timezone_get());
 define('RESTpAPIABSPATH', dirname(__FILE__) . '/');
 
