7567666fa87d13204169043dc09b6ef1350cf0b8
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-03 09:53:54 -0300

    up

--- app/Console/Commands/CrudControllerCommand.php
+++ app/Console/Commands/CrudControllerCommand.php
@@ -64,8 +64,8 @@ class CrudControllerCommand extends Command
                 </div>
             </section>
         </div>
-        <div id="pdocrud-ajax-loader">
-            <img width="300" src="<?=$_ENV["BASE_URL"]?>app/libs/artify/images/ajax-loader.gif" class="pdocrud-img-ajax-loader"/>
+        <div id="artify-ajax-loader">
+            <img width="300" src="<?=$_ENV["BASE_URL"]?>app/libs/artify/images/ajax-loader.gif" class="artify-img-ajax-loader"/>
         </div>
         <?php require "layouts/footer.php"; ?>';
 
