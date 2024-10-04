e44cd229788f573478c67cfce46613aa5a75296f
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-04 09:44:27 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1419,7 +1419,7 @@ class HomeController
 			$_ENV["ENABLE_JWTAUTH"] = false;
 		}
 
-		$config->setPK("id_configuraciones_modulos");
+		//$config->setPK("id_configuraciones_modulos");
 		$config->set_template($html_template_config);
 		$config->fieldCssClass("generar_jwt_token", array("generar_jwt_token"));
 		$config->fieldCssClass("autenticar_jwt_token", array("autenticar_jwt_token"));
@@ -1431,6 +1431,8 @@ class HomeController
 		$config->fieldNotMandatory("autenticar_jwt_token");
 		$config->fieldValidationType("tiempo_caducidad_token", "required", "", "Ingrese un tiempo de Caducidad para el token");
 		$config->setSettings("refresh", false);
+		$config->setSettings("editbtn", true);
+		$config->setSettings("delbtn", true);
 		$config->fieldTypes("logo_pdf", "FILE_NEW");
 		$config->fieldTypes("marca_agua_pdf", "FILE_NEW");
 		$config->fieldTypes("generar_jwt_token", "select");
@@ -1441,7 +1443,7 @@ class HomeController
 		$config->fieldDataAttr("autenticar_jwt_token", array("disabled"=>"disabled"));
 		$config->fieldGroups("Name1",array("logo_pdf","marca_agua_pdf"));
 		$config->fieldGroups("Name2",array("generar_jwt_token","autenticar_jwt_token"));
-		$render_conf = $config->dbTable("configuraciones_modulos")->render("editform", array("id" => "1"));
+		$render_conf = $config->dbTable("configuraciones_modulos")->render();
 
 		View::render(
 			"modulos",
