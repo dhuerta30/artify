cce4bbd7efdf4555032d049b98e5158374842834
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-10 14:51:04 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1337,7 +1337,7 @@ class HomeController
 		$artify->fieldTypes("mostrar_campos_filtro", "multiselect");
 		
 		$artify->fieldTypes("tipo_de_filtro", "multiselect");
-		$artify->fieldDataBinding("tipo_de_filtro", array("radio" => "radio", "dropdown" => "dropdown"), "", "", "array");
+		$artify->fieldDataBinding("tipo_de_filtro", array("radio" => "radiobox", "dropdown" => "Combobox", "date" => "fecha", "text" => "texto"), "", "", "array");
 
 		$artify->fieldTypes("mostrar_campos_formulario_editar", "multiselect");
 		$artify->fieldTypes("ordenar_grilla_por", "select");
