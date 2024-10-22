6f7e4c7476c859454f55fd0243631440ecdbf393
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-22 15:38:41 -0300

    up

--- app/Services/CrudService.php
+++ app/Services/CrudService.php
@@ -190,7 +190,7 @@ class CrudService
             if(!empty($file_callback)){
                 $fileGenerator = new CrudService();
 
-                $fileName = $file_callback.".php";
+                $fileName = $file_callback . ".php";
                 $phpCode = 'echo "Este es un archivo PHP generado dinámicamente.";';
 
                 // Llamar al método principal para crear el archivo y escribir el código
