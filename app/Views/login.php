130293ed6e44cc2dba67a2867ba1173a79e6cfc1
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-10-22 08:55:50 -0300

    up

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,6 +7,7 @@
 	<title><?=$_ENV["APP_NAME"]?> | Login</title>
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="<?=$_ENV["BASE_URL"]?>theme/plugins/fontawesome-free/css/all.min.css">
 </head>
 <body>
 <style>
